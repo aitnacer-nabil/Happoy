918d0aa03a173ec0d165a0a521ba168bf0a4ba71
aitnacer-nabil <palancia.gor@gmail.com>
2024-04-02 01:54:34 +0000

    Create Eureka service and add Eureka client discovery to both services

--- Category-Service/src/main/java/com/nabilaitnacer/categoryservice/CategoryServiceApplication.java
+++ Category-Service/src/main/java/com/nabilaitnacer/categoryservice/CategoryServiceApplication.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
@@ -25,6 +26,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 )
         )
 )
+@EnableDiscoveryClient
 public class CategoryServiceApplication {
 
     public static void main(String[] args) {
